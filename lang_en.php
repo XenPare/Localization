e458f266a281574bcc067ece00dfe7266144b638
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-08-03 23:56:53 +0700

    tabs

--- lang_en.php
+++ lang_en.php
@@ -1,36 +1,36 @@
 <?php
 	$LANG = array(
-        "home" => "Home",
-        "account" => "Account",
-        "news" => "News",
+		"home" => "Home",
+		"account" => "Account",
+		"news" => "News",
 		"donate" => "Donate",
 		"language" => "Language",
 		"profile" => "Profile",
 		"login" => "Log in",
-        "logout" => "Log out",
-        
+		"logout" => "Log out",
+
 		"who" => "Who we are ?",
-        "host" => "Garry's mod servers we host",
+		"host" => "Garry's mod servers we host",
 
-        "years" => "Years",
-        "days" => "Days",
-        "hours" => "Hours",
-        "minutes" => "Minutes",
+		"years" => "Years",
+		"days" => "Days",
+		"hours" => "Hours",
+		"minutes" => "Minutes",
 
-        "working" => "We are keep working for",
-        "developers" => "Innovators",
-        "gamers" => "Participants",
-        "enthusiasts" => "Enthusiasts",
+		"working" => "We are keep working for",
+		"developers" => "Innovators",
+		"gamers" => "Participants",
+		"enthusiasts" => "Enthusiasts",
 
-        "account_created" => "Account created",
-        "last_online" => "Last online",
-        "real_name" => "Real name",
-        "region" => "Region",
-        
-        "region_info" => "The region is auto-selected by your country",
+		"account_created" => "Account created",
+		"last_online" => "Last online",
+		"real_name" => "Real name",
+		"region" => "Region",
 
-        "warning" => "Warning",
-        "warning_txt" => "All news are auto translated, expect the mistakes.",
+		"region_info" => "The region is auto-selected by your country",
+
+		"warning" => "Warning",
+		"warning_txt" => "All news are auto translated, expect the mistakes.",
 
 		"0" => "Offline",
 		"1" => "Online",
@@ -38,27 +38,27 @@
 		"3" => "Away",
 		"4" => "Snooze",
 
-        "gamers_desc" => "We enjoy playing games with you.",
-        "developers_desc" => "We enjoy creating new and interesting things.",
-        "enthusiasts_desc" => "
-            <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>We enjoy creating our own things and playing around with them.
-            <br>
-            <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>We are happy to host different gamemodes and be a place of spending time for every player we have.
-            <br>
-            <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>You won't find any imbalanced or snippy donation on our projects.
-            <br>
-            <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>We never have any certain plans or something.
-            <br>
-            <br>
-            You are safe here<i class='fa fa-check has-text-dark' aria-hidden='true'></i>
-        ",
+		"gamers_desc" => "We enjoy playing games with you.",
+		"developers_desc" => "We enjoy creating new and interesting things.",
+		"enthusiasts_desc" => "
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>We enjoy creating our own things and playing around with them.
+		    <br>
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>We are happy to host different gamemodes and be a place of spending time for every player we have.
+		    <br>
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>You won't find any imbalanced or snippy donation on our projects.
+		    <br>
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>We never have any certain plans or something.
+		    <br>
+		    <br>
+		    You are safe here<i class='fa fa-check has-text-dark' aria-hidden='true'></i>
+		",
 
-        "ttt_desc" => 'The gamemode is about a group of "terrorists" who have traitors among them, out to kill everyone who is not a traitor.',
-        "gc_desc" => "This is a gamemode that focuses heavily on teamwork and lethality of combat.",
-        "xprp_desc" => "This is a based on DarkRP gamemode of a simulation of urban life.",
+		"ttt_desc" => 'The gamemode is about a group of "terrorists" who have traitors among them, out to kill everyone who is not a traitor.',
+		"gc_desc" => "This is a gamemode that focuses heavily on teamwork and lethality of combat.",
+		"xprp_desc" => "This is a based on DarkRP gamemode of a simulation of urban life.",
 
-        "join" => "Join",
+		"join" => "Join",
 
-        "media" => "Our social media"
+		"media" => "Our social media"
 	);
 ?>
