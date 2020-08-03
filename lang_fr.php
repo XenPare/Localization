2eb8d8b64f2f197f290f246c29681cbabfbce0d0
crester <35332822+cresterienvogel@users.noreply.github.com>
2020-08-03 23:56:25 +0700

    tabs

--- lang_fr.php
+++ lang_fr.php
@@ -1,36 +1,36 @@
 <?php
 	$LANG = array(
-        "home" => "Accueil",
-        "account" => "Compte",
-        "news" => "Actualités",
+		"home" => "Accueil",
+		"account" => "Compte",
+		"news" => "Actualités",
 		"donate" => "Donation",
 		"language" => "Langage",
 		"profile" => "Profil",
 		"login" => "Connexion",
-        "logout" => "Déconnexion",
-        
+		"logout" => "Déconnexion",
+
 		"who" => "Qui sommes-nous ?",
-        "host" => "Les serveurs Garry's mod que nous hébergeons",
-        
-        "years" => "Ans",
-        "days" => "Jours",
-        "hours" => "Heures",
-        "minutes" => "Minutes",
+		"host" => "Les serveurs Garry's mod que nous hébergeons",
 
-        "working" => "Nous travaillons depuis",
-        "developers" => "Innovateurs",
-        "gamers" => "Participants",
-        "enthusiasts" => "Enthousiastes",
+		"years" => "Ans",
+		"days" => "Jours",
+		"hours" => "Heures",
+		"minutes" => "Minutes",
 
-        "account_created" => "Compte créé",
-        "last_online" => "Dernière connexion",
-        "real_name" => "Nom réel",
-        "region" => "Région",
+		"working" => "Nous travaillons depuis",
+		"developers" => "Innovateurs",
+		"gamers" => "Participants",
+		"enthusiasts" => "Enthousiastes",
 
-        "region_info" => "La région est sélectionnée automatiquement par votre pays",
+		"account_created" => "Compte créé",
+		"last_online" => "Dernière connexion",
+		"real_name" => "Nom réel",
+		"region" => "Région",
 
-        "warning" => "Avertissement",
-        "warning_txt" => "Toutes les nouvelles sont traduites automatiquement, attendez-vous aux erreurs.",
+		"region_info" => "La région est sélectionnée automatiquement par votre pays",
+
+		"warning" => "Avertissement",
+		"warning_txt" => "Toutes les nouvelles sont traduites automatiquement, attendez-vous aux erreurs.",
 
 		"0" => "Hors ligne",
 		"1" => "En ligne",
@@ -38,27 +38,27 @@
 		"3" => "Absent",
 		"4" => "Endormi",
 
-        "gamers_desc" => "Nous aimons jouer avec vous.",
-        "developers_desc" => "Nous aimons créer des choses nouvelles et intéressantes.",
-        "enthusiasts_desc" => "
-            <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>Nous aimons créer nos propres choses et jouer avec.
-            <br>
-            <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>Nous sommes heureux d'accueillir différents modes de jeu et d'être un lieu de loisirs pour chaque joueur que nous avons.
-            <br>
-            <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>Vous ne trouverez pas de contreparties abusive sur notre système de donation utilisé sur nos projets.
-            <br>
-            <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>Nous n'avons jamais de projets précis ou quelque chose du genre.
-            <br>
-            <br>
-            Vous êtes en sécurité ici<i class='fa fa-check has-text-dark' aria-hidden='true'></i>
-        ",
+		"gamers_desc" => "Nous aimons jouer avec vous.",
+		"developers_desc" => "Nous aimons créer des choses nouvelles et intéressantes.",
+		"enthusiasts_desc" => "
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>Nous aimons créer nos propres choses et jouer avec.
+		    <br>
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>Nous sommes heureux d'accueillir différents modes de jeu et d'être un lieu de loisirs pour chaque joueur que nous avons.
+		    <br>
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>Vous ne trouverez pas de contreparties abusive sur notre système de donation utilisé sur nos projets.
+		    <br>
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>Nous n'avons jamais de projets précis ou quelque chose du genre.
+		    <br>
+		    <br>
+		    Vous êtes en sécurité ici<i class='fa fa-check has-text-dark' aria-hidden='true'></i>
+		",
 
-        "ttt_desc" => 'Le jeu met en scène un groupe de "terroristes" qui comptent parmi eux des traîtres et qui cherchent à tuer tous ceux qui ne sont pas des traîtres.',
-        "gc_desc" => "Il s'agit d'un mode de jeu qui met fortement l'accent sur le travail d'équipe et la mortalité des combats.",
-        "xprp_desc" => "L'idée de base de ce mode de jeu basé sur DarkRP est une simulation de la vie urbaine.",
+		"ttt_desc" => 'Le jeu met en scène un groupe de "terroristes" qui comptent parmi eux des traîtres et qui cherchent à tuer tous ceux qui ne sont pas des traîtres.',
+		"gc_desc" => "Il s'agit d'un mode de jeu qui met fortement l'accent sur le travail d'équipe et la mortalité des combats.",
+		"xprp_desc" => "L'idée de base de ce mode de jeu basé sur DarkRP est une simulation de la vie urbaine.",
 
-        "join" => "Rejoindre",
+		"join" => "Rejoindre",
 
-        "media" => "Nos réseaux sociaux"
+		"media" => "Nos réseaux sociaux"
 	);
 ?>
