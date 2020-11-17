42d82a1f96fc142871397bf30311e6abbb7471bb
crester <druggygangsta@gmail.com>
2020-11-18 00:27:16 +0700

    files transfer

---
+++ web/lang_en.php
@@ -0,0 +1,95 @@
+<?php
+	$LANG = array(
+		"home" => "Home",
+		"account" => "Account",
+		"news" => "News",
+		"donate" => "Donate",
+		"language" => "Language",
+		"profile" => "Profile",
+		"login" => "Log in",
+		"logout" => "Log out",
+		"donators" => "Peacekeepers",
+
+		"who" => "Who we are ?",
+		"host" => "Garry's mod servers we host",
+
+		"years" => "Years",
+		"days" => "Days",
+		"hours" => "Hours",
+		"minutes" => "Minutes",
+
+		"working" => "We are keep working for",
+		"developers" => "Innovators",
+		"gamers" => "Participants",
+		"enthusiasts" => "Enthusiasts",
+
+		"account_created" => "Account created",
+		"last_online" => "Last online",
+		"real_name" => "Real name",
+		"region" => "Region",
+
+		"region_info" => "The region is auto-selected by your country",
+
+		"warning" => "Warning",
+
+		"0" => "Offline",
+		"1" => "Online",
+		"2" => "Busy",
+		"3" => "Away",
+		"4" => "Snooze",
+
+		"gamers_desc" => "We enjoy playing games with you.",
+		"developers_desc" => "We enjoy creating new and interesting things.",
+		"enthusiasts_desc" => "
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>We enjoy creating our own things and playing around with them.
+		    <br>
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>We are happy to host different gamemodes and be a place of spending time for every player we have.
+		    <br>
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>You won't find any imbalanced or snippy donation on our projects.
+		    <br>
+		    <i class='fa fa-caret-right has-text-dark' aria-hidden='true'></i>We never have any certain plans or something.
+		    <br>
+		    <br>
+		    You are safe here<i class='fa fa-check has-text-dark' aria-hidden='true'></i>
+		",
+
+		"ttt_desc" => 'The gamemode is about a group of "terrorists" who have traitors among them, out to kill everyone who is not a traitor.',
+		"gc_desc" => "This is a gamemode that focuses heavily on teamwork and lethality of combat.",
+		"xprp_desc" => "This is a based on DarkRP gamemode of a simulation of urban life.",
+		"rust_desc" => "This is a multuplayer survival game.",
+
+		"join" => "Join",
+
+		"media" => "Our social media",
+
+		"peacekeepers1" => "Since the second half of 2020, we no longer accept or provide donation services on our servers.",
+		"peacekeepers2" => "Here are the people who have contributed to the community.",
+
+		"error_login" => "Please, log in.",
+		"error" => "Error !",
+
+		"forum" => "Forum",
+		"forum_new" => "New thread",
+		"forum_send" => "Send",
+		"forum_warning" => "Please, do not insult another forum members, otherwise you have a risk to get banned on the forum.",
+
+		"post_warning" => "You won't be able to delete your thread.",
+
+		"category" => "Category",
+		"done" => "Done !",
+
+		"title" => "Title",
+		"text" => "Write something",
+
+		"messages" => "messages",
+		"threads" => "threads",
+		"at" => "at",
+
+		"general" => "<i class='fa fa-asterisk has-text-white' aria-hidden='true'></i> General",
+		"reports" => "<i class='fa fa-legal has-text-white' aria-hidden='true'></i> Reports",
+		"appeals" => "<i class='fa fa-hand-stop-o has-text-white' aria-hidden='true'></i> Appeals",
+		"applications" => "<i class='fa fa-user-plus has-text-white' aria-hidden='true'></i> Applications",
+		"suggestions" => "<i class='fa fa-mortar-board has-text-white' aria-hidden='true'></i> Suggestions",
+		"issues" => "<i class='fa fa-warning has-text-dark' aria-hidden='true'></i> Issues"
+	);
+?>
